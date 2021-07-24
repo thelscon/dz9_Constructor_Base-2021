ad5d8f6bca953d7b2011e02da0ffb2c083ac5836
thelscon <thelscon@gmail.com>
2021-07-24 20:17:33 +0300

    v3

--- script.js
+++ script.js
@@ -1,75 +1,79 @@
 'use strict' ;
 
-const PERMISSION_GANDER = [ 'male' , 'female' , 'non-binary' , 'transgender' , 'intersex' , 'n/a' ] ;
+function Person ( name , age , gander ) {
+    if ( !new.target ) return ;
 
-function secretFunctionName ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) value = currentValue.trim() ;
-        return value ;
-    } ;
-}
-const _name = secretFunctionName () ;
+    const PERMISSION_GANDER = [ 'male' , 'female' , 'non-binary' , 'transgender' , 'intersex' , 'n/a' ]
 
-function secretFunctionAge ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) {
-            currentValue = parseInt ( currentValue ) ;
-            if( currentValue > 0 && currentValue <= 120 ) {
-                value = currentValue ;
-            }
-        } 
-        return value ;
-    } ;
-}
-const _age = secretFunctionAge () ;    
-
-function secretFunctionGander ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) {
-            currentValue = currentValue.trim().toLowerCase() ;
-            if( PERMISSION_GANDER.includes( currentValue ) ) value = currentValue ;
-        } 
-        return value ;
-    } ;
-}
-const _gander = secretFunctionGander () ;
-
-function Person () {                            //инициализируем конструктор
-
-    if ( !new.target ) return ;  
-
-}
-Object.defineProperties ( Person.prototype , {  //инициализируем прототип
-    'name' : {
-        get : function () { return _name () } ,
-        set : function ( value ) { _name ( value ) } ,
-        enumerable : false ,
-        configurable : false
-    } ,
-    'age' : {
-        get : function () { return _age () } ,
-        set : function ( value ) { _age ( value ) } ,
-        enumerable : false ,
-        configurable : false
-    } ,
-    'gander' : {
-        get : function () { return _gander () } ,
-        set : function ( value ) { _gander ( value ) } ,
-        enumerable : false ,
-        configurable : false
+    const METH_NAME = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) value = currentValue.trim() ;
+            return value ;
+        } ;
     }
-} ) ;
+    const PROP_NAME = METH_NAME () ;
 
+    const METH_AGE = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) {
+                currentValue = parseInt ( currentValue ) ;
+                if( currentValue > 0 && currentValue <= 120 ) {
+                    value = currentValue ;
+                }
+            } 
+            return value ;
+        } ;
+    }
+    const PROP_AGE = METH_AGE () ;
 
-const newPerson = new Person () ; 
+    const METH_GANDER = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) {
+                currentValue = currentValue.trim().toLowerCase() ;
+                if( PERMISSION_GANDER.includes( currentValue ) ) value = currentValue ;
+            } 
+            return value ;
+        } ;
+    }
+    const PROP_GANDER = METH_GANDER () ;
 
-newPerson.name = ' bob' ;
-newPerson.gander = ' Male' ;
-newPerson.age = 120 ;
-console.dir ( newPerson ) ;
+    Object.defineProperties ( this , {
+        'name' : {
+            get : function () { return PROP_NAME () } ,
+            set : function ( value ) { PROP_NAME ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        } ,
+        'age' : {
+            get : function () { return PROP_AGE () } ,
+            set :function ( value  ) { PROP_AGE ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        } ,
+        'gander' : {
+            get : function () { return PROP_GANDER () } ,
+            set : function ( value ) { PROP_GANDER ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        }
+    } ) ;
+
+    this.name = name ;
+    this.age = age ;
+    this.gander = gander ;
+
+}                            //инициализируем конструктор
+
+const newPerson = new Person ( ' bob' , 120 , ' Male' ) ; 
+console.log ( newPerson.name ) ;
+console.log ( newPerson.age ) ;
+newPerson.name = 'ivan' ;
+newPerson.age = 121 ;
+console.dir ( newPerson.name ) ;
+console.log ( newPerson.male ) ;
 
 /* Object.defineProperty ( newPerson , 'age' , { //нельзя изменить поле объекта age
     configurable : true ,
